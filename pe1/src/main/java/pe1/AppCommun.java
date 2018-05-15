19d02000dd13e636f43c1cc794c99b11bd0aa6bd
Dib Abdallah <boudidib@live.com>
2018-05-15 12:06:16 +0200

    bou

--- pe1/src/main/java/pe1/AppCommun.java
+++ pe1/src/main/java/pe1/AppCommun.java
@@ -5,6 +5,7 @@ public class AppCommun {
 	public static void main(String[] args) {
 		System.out.println("app commun abdallah");
 		System.out.println("app commun didier");
+		System.out.println("boudi");
 		
 
 	}
