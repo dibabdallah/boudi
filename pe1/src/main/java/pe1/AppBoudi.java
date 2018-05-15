af6896d82840c2ef85a56613f8f0a7c46b3f63cb
Dib Abdallah <boudidib@live.com>
2018-05-15 11:16:52 +0200

    ***

--- pe1/src/main/java/pe1/AppBoudi.java
+++ pe1/src/main/java/pe1/AppBoudi.java
@@ -5,6 +5,7 @@ public class AppBoudi {
 	public static void main(String[] args) {
 		System.out.println("boudi developp");
 		System.out.println("developp 15");
+		System.out.println("*****");
 	}
 
 }
